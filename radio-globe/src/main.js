1396ae55e6837ead633d253140effd7559ccb5e7
quliyevislam <9337@holbertonstudents.com>
2025-05-31 01:53:02 +0400

    **Star field**

--- radio-globe/src/main.js
+++ radio-globe/src/main.js
@@ -28,10 +28,6 @@ window.addEventListener("resize", () => {
 });
 
 
-
-
-
-
 // GLOBE
 const globe = new THREE.Mesh(
     new THREE.SphereGeometry(1, 50, 50),
@@ -51,7 +47,7 @@ const testGlobe1 = new THREE.Mesh(
 );
 scene.add(testGlobe1);
 
-
+// STARS
 const testGlobe2 = new THREE.Mesh(
     new THREE.SphereGeometry(3, 50, 50),
     new THREE.MeshBasicMaterial({
@@ -62,7 +58,41 @@ const testGlobe2 = new THREE.Mesh(
 scene.add(testGlobe2);
 
 
+const getRandomPoints = (r1, r2, count) => {
+    const vertices = new Float32Array(count * 3);
+    let x, y, z;
+    let angle1, angle2;
+    let distance, projection;
 
+    for (let i = 0; i < count * 3; i += 3) {
+        angle1 = Math.random() * 2 * Math.PI;
+        angle2 = Math.random() * 2 * Math.PI;
+        distance = r1 + Math.random() * (r2 - r1);
+        projection = distance * Math.cos(angle1);
+        y = distance * Math.sin(angle1);
+        z = projection * Math.cos(angle2);
+        x = projection * Math.sin(angle2);
+        vertices[i] = x;
+        vertices[i + 1] = y;
+        vertices[i + 2] = z;
+    }
+
+    return vertices;
+}
+const starField = new THREE.Points(
+    new THREE.BufferGeometry().setAttribute(
+        "position",
+        new THREE.BufferAttribute(
+            getRandomPoints(2, 3, 400000),
+            3
+        )
+    ),
+    new THREE.PointsMaterial({
+        size: 0.005,
+        color: 0xffffff
+    })
+);
+scene.add(starField);
 
 const animate = () => {
     renderer.render(scene, camera);
